d02201882ba41cc3907f89ca14fda53137a49225
yuchinnnnn <145102391+yuchinnnnn@users.noreply.github.com>
2025-06-23 06:22:55 +0800

    upload navbar.js

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const accountLink = document.getElementById("accountLink");
   const username = localStorage.getItem("username");
+  const token = localStorage.getItem("token");
 
-  if (username && accountLink) {
-    accountLink.textContent = username;
-    accountLink.href = "/account";
+  if (accountLink) {
+    if (username && token) {
+      accountLink.textContent = username;
+      accountLink.href = "/account";
+    } else {
+      accountLink.textContent = "Login / Sign Up";
+      accountLink.href = "/login";
+    }
   }
 });
